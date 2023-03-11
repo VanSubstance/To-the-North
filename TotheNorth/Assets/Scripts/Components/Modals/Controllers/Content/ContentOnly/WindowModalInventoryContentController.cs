d0ff5abdc08dfa1928a6223e33ac80ebfe9915a3
MJ <75807617+kang123077@users.noreply.github.com>
2023-03-12 00:32:18 +0900

    Fix: Init Inventory Slots
    #29
    ~ WindowModalInventoryContentController 수정
    + InventorySlot 추가

--- TotheNorth/Assets/Scripts/Components/Modals/Controllers/Content/ContentOnly/WindowModalInventoryContentController.cs
+++ TotheNorth/Assets/Scripts/Components/Modals/Controllers/Content/ContentOnly/WindowModalInventoryContentController.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
-using TMPro;
 
-public class WindowModalInventoryContentController : AWindowModalController<ModalContentTestStat>
+public class WindowModalInventoryContentController : AWindowModalController<InventorySlot>
 {
     private bool isInit = false;
+    private InventorySlot[,] slots = new InventorySlot[10, 13];
     public sealed override void ClearContent()
     {
     }
@@ -11,12 +11,24 @@ public class WindowModalInventoryContentController : AWindowModalController<Moda
     public sealed override void InitCompositionByType()
     {
         if (isInit) return;
-        Transform temp = base.GetContentContainerTf();
+        Transform temp = base.GetContentContainerTf().GetChild(0);
+        int idx = 0;
+        for (int col = 0; col < 13; col++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                slots[row, col] = temp.GetChild(idx).GetComponent<InventorySlot>();
+                slots[row, col].row = row;
+                slots[row, col].column = col;
+                idx++;
+            }
+        }
         isInit = true;
     }
 
-    public sealed override void InitContentByType(ModalContentTestStat contentToInit)
+    public sealed override void InitContentByType(InventorySlot contentToInit)
     {
+        if (isInit == false) InitCompositionByType();
         ClearContent();
     }
 }
---
+++ TotheNorth/Assets/Scripts/Components/Modals/Objects/InventorySlot.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    // public Image slotImage;
+    public int row;
+    public int column;
+}
