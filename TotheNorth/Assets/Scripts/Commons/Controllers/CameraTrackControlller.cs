7c8bd6b9d28c0dd9fd6a566bbe1ba7fd712ba8dd
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-03-31 17:31:35 +0900

    Closes #75: Feature/sight control (#76)
    * Refactor: Moving camera to the mouse position
    
    #75
    
    ~ 구조 변경: 화면 왜곡 벡터로 유저 이외의 이동 관리
    
    * Fix: Smooth Transition of Camera
    
    #75
    
    ~ 카메라 이동 Translate 적용
    ~ 시선 집중 시 이동속도 변경: 0.5 -> 1.5

--- TotheNorth/Assets/Scripts/Commons/Controllers/CameraTrackControlller.cs
+++ TotheNorth/Assets/Scripts/Commons/Controllers/CameraTrackControlller.cs
@@ -6,17 +6,21 @@ using UnityEngine;
 
 public class CameraTrackControlller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static Vector3
+        targetDir = Vector3.zero;
+
     void Start()
     {
     }
 
     private void LateUpdate()
     {
-        Vector3 curDistortionVector = CalculationFunctions.DirFromAngle(InGameStatus.User.Movement.curdegree) * (InGameStatus.User.Detection.Sight.range - InGameStatus.User.Detection.Sight.rangeMin);
-        transform.position = new Vector3(
-            GlobalComponent.Common.userTf.position.x,
-            GlobalComponent.Common.userTf.position.y,
-            -20f) + curDistortionVector;
+        transform.Translate(
+            new Vector2(
+                GlobalComponent.Common.userTf.position.x - transform.position.x + targetDir.x,
+                GlobalComponent.Common.userTf.position.y - transform.position.y + targetDir.y
+                )
+                * Time.deltaTime * 2
+            );
     }
 }
