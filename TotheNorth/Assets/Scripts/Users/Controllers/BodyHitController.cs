0a17e9f24b2a2cce6e188a96e2ef0985392178f5
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-04-01 19:47:32 +0900

    Add: Seperation of Body into indicated parts
    #80
    
    + 신체 구조 고안중 ...
    // 나눔은 기본적으로 완료, 추후 히트박스 관련 이벤트 컨트롤러 기본 고안 필요

---
+++ TotheNorth/Assets/Scripts/Users/Controllers/BodyHitController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Users.Controllers
+{
+    class BodyHitController : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+
+    }
+}
