afb0ffa8e6cdb886443983636748cdcde0d4285c
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-05-19 20:27:25 +0900

    Add: Mouse Moveable Controller
    #203
    
    + 마우스로 이동 가능한 컨트롤러 구현

--- To-the-North-3d/Assets/Scripts/Commons/Controllers/MouseInteraction/MouseInteractionMoveableController.cs
+++ To-the-North-3d/Assets/Scripts/Commons/Controllers/MouseInteraction/MouseInteractionMoveableController.cs
@@ -4,19 +4,24 @@ namespace Assets.Scripts.Commons
 {
     public class MouseInteractionMoveableController : MouseInteractionController
     {
+        private Vector3 correctionVector;
         public override void ActionMouseDown()
         {
-            Debug.Log($"Down");
+            Vector3 startPos = UIManager.Instance.transform.InverseTransformPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            correctionVector = transform.localPosition - startPos;
         }
 
         public override void ActionMouseDrag()
         {
-            Debug.Log($"Drag");
+            Vector3 nextPos = UIManager.Instance.transform.InverseTransformPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            transform.localPosition = new Vector3(
+                nextPos.x + correctionVector.x,
+                nextPos.y + correctionVector.y,
+                0);
         }
 
         public override void ActionMouseUp()
         {
-            Debug.Log($"Up");
         }
     }
 }
