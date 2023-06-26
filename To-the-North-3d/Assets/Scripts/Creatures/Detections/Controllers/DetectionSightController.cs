c5b8f6e3225b7c5f52069ecc0feb97586a282c30
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-06-26 13:27:16 +0900

    Refactor: Battles
    #221
    
    ~ namespace Battles 관련 Update 분리 처리

--- To-the-North-3d/Assets/Scripts/Creatures/Detections/Controllers/DetectionSightController.cs
+++ To-the-North-3d/Assets/Scripts/Creatures/Detections/Controllers/DetectionSightController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Assets.Scripts.Commons;
 using Assets.Scripts.Commons.Functions;
 using Assets.Scripts.Events;
 using UnityEngine;
